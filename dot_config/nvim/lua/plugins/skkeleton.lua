b0a1abc703f30a4c98b6fcbd5d7c766c76021083
susiyaki <susiyaki.dev@gmail.com>
2023-10-11 14:51:44 +0900

    fix: skkeleton command

--- dot_config/nvim/lua/plugins/skkeleton.lua
+++ dot_config/nvim/lua/plugins/skkeleton.lua
@@ -1,8 +1,13 @@
 local utils = require('utils')
 
+utils.map('i', '<C-j>', '<Plug>(skkeleton-enable)', { noremap = false })
 utils.map('i', 'jk', '<Plug>(skkeleton-enable)', { noremap = false })
-utils.map('c', 'jk', '<Plug>(skkeleton-disalbed)', { noremap = false })
+-- 子音があると1文字目だけうまく動かないので、1回打って消して回避している
+utils.map('c', '<C-j>', '<Plug>(skkeleton-enable)ka<DEL>', { noremap = false, silent = true })
+utils.map('c', 'jk', '<Plug>(skkeleton-enable)ka<DEL>', { noremap = false, silent = true })
+
 utils.map('i', 'jj', '<ESC>')
+
 
 vim.cmd([[
 function! s:skkeleton_init() abort
