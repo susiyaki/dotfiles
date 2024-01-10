75f6152284b5ef900613fd98d70a12316e6f87d0
susiyaki <susiyaki.dev@gmail.com>
2024-01-10 12:43:58 +0900

    chroe: update nvim config

--- dot_config/nvim/lua/plugins/lsp/mason_lspconfig.lua
+++ dot_config/nvim/lua/plugins/lsp/mason_lspconfig.lua
@@ -30,27 +30,10 @@ capabilities.textDocument.completion.completionItem.snippetSupport = true;
 mason_lspconfig.setup_handlers({ function(server)
   local opts = { capabilities = capabilities, on_attach = on_attach }
 
-  local node_root_dir = nvim_lsp.util.root_pattern("package.json")
-  local is_node_repo = node_root_dir(vim.api.nvim_buf_get_name(0)) ~= nil
-
   if server == "tsserver" then
-    if not is_node_repo then
-      return
-    end
-
-    opts.root_dir = node_root_dir
-  elseif server == "eslint" then
-    if not is_node_repo then
-      return
-    end
-
-    opts.root_dir = node_root_dir
+    opts.root_dir = nvim_lsp.util.root_pattern("package.json")
   elseif server == "denols" then
-    if is_node_repo then
-      return
-    end
-
-    opts.root_dir = nvim_lsp.util.root_pattern("deno.json", "deno.jsonc", "deps.ts", "import_map.json")
+    opts.root_dir = nvim_lsp.util.root_pattern("deno.json")
     opts.init_options = {
       lint = true,
       unstable = true,
