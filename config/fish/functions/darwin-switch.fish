74531a6b4da27bc2b9d6539d19154b4db87ffa56
susiyaki <susiyaki.dev@gmail.com>
2026-01-13 14:40:39 +0900

    update

--- config/fish/functions/darwin-switch.fish
+++ config/fish/functions/darwin-switch.fish
@@ -1,5 +1,4 @@
 function darwin-switch
     cd ~/dotfiles-new
     nix build .#darwinConfigurations.m1-mac.system && sudo ./result/sw/bin/darwin-rebuild switch --flake ~/dotfiles-new#m1-mac
-    cd -
 end
--- config/nvim/lua/plugins/lsp/settings.lua
+++ config/nvim/lua/plugins/lsp/settings.lua
@@ -17,6 +17,21 @@ M.on_attach = function(client, bufnr)
       callback = vim.lsp.buf.clear_references,
     })
   end
+
+  -- Rustファイル保存時にLSPを即座に更新
+  if client.name == 'rust_analyzer' then
+    vim.api.nvim_create_autocmd('BufWritePost', {
+      buffer = bufnr,
+      callback = function()
+        -- didSaveイベントを明示的に送信
+        vim.lsp.buf_notify(bufnr, 'textDocument/didSave', {
+          textDocument = {
+            uri = vim.uri_from_bufnr(bufnr),
+          },
+        })
+      end,
+    })
+  end
 end
 
 return M
