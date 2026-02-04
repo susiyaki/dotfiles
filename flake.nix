988dda391c141f57f8ab7eb9281663e03d157054
susiyaki <susiyaki.dev@gmail.com>
2026-02-04 20:42:45 +0900

    update

--- flake.nix
+++ flake.nix
@@ -23,58 +23,66 @@
     };
   };
 
-  outputs = { self, nixpkgs, nix-darwin, home-manager, ... }@inputs: {
-    # macOS system (nix-darwin)
-    darwinConfigurations = {
-      m1-mac = nix-darwin.lib.darwinSystem {
-        system = "aarch64-darwin";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/m1-mac
-          home-manager.darwinModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              users.laeno = import ./home/darwin.nix;
-              extraSpecialArgs = { inherit inputs; };
-            };
-          }
-        ];
+  outputs = { self, nixpkgs, nix-darwin, home-manager, ... }@inputs:
+    let
+      supportedSystems = [ "aarch64-darwin" "x86_64-linux" ];
+      forAllSystems = function: nixpkgs.lib.genAttrs supportedSystems (system: function system);
+    in
+    {
+      # macOS system (nix-darwin)
+      darwinConfigurations = {
+        m1-mac = nix-darwin.lib.darwinSystem {
+          system = "aarch64-darwin";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/m1-mac
+            home-manager.darwinModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                users.laeno = import ./home/darwin.nix;
+                extraSpecialArgs = { inherit inputs; };
+              };
+            }
+          ];
+        };
       };
-    };
 
-    # Linux system (NixOS)
-    nixosConfigurations = {
-      thinkpad-p14s = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./hosts/thinkpad-p14s
-          home-manager.nixosModules.home-manager
-          {
-            home-manager = {
-              useGlobalPkgs = true;
-              useUserPackages = true;
-              backupFileExtension = "backup";
-              users.susiyaki = import ./home/linux.nix;
-              extraSpecialArgs = { inherit inputs; };
-            };
-          }
-        ];
+      # Linux system (NixOS)
+      nixosConfigurations = {
+        thinkpad-p14s = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+          modules = [
+            ./hosts/thinkpad-p14s
+            home-manager.nixosModules.home-manager
+            {
+              home-manager = {
+                useGlobalPkgs = true;
+                useUserPackages = true;
+                backupFileExtension = "backup";
+                users.susiyaki = import ./home/linux.nix;
+                extraSpecialArgs = { inherit inputs; };
+              };
+            }
+          ];
+        };
       };
-    };
 
-    # Standalone Home Manager (optional)
-    homeConfigurations = {
-      "laeno@m1-mac" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.aarch64-darwin;
-        modules = [ ./home/darwin.nix ];
+      # Standalone Home Manager (optional)
+      homeConfigurations = {
+        "laeno@m1-mac" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.aarch64-darwin;
+          modules = [ ./home/darwin.nix ];
+        };
+        "susiyaki@thinkpad-p14s" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+          modules = [ ./home/linux.nix ];
+        };
       };
-      "susiyaki@thinkpad-p14s" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-        modules = [ ./home/linux.nix ];
-      };
+
+      # Formatter for nix fmt
+      formatter = forAllSystems (system: nixpkgs.legacyPackages.${system}.nixpkgs-fmt);
     };
-  };
 }
